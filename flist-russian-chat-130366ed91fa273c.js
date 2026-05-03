ee3cc1e907926f1ceb5eb999117e269ed84e8c22
bestrigyn <darius70000@gmail.com>
2026-05-03 20:03:18 +0000

    Deploying to gh-pages from @ bestrigyn/F-list-RUSSIAN_CHAT_VERSIONS@430227a1ca6a2b6f9ddc5bd45e509a48f8523914 🚀

--- flist-russian-chat-130366ed91fa273c.js
+++ flist-russian-chat-130366ed91fa273c.js
@@ -431,7 +431,7 @@ function __wbg_get_imports() {
             return ret;
         },
         __wbindgen_cast_0000000000000004: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [NamedExternref("MouseEvent")], shim_idx: 13, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [NamedExternref("MouseEvent")], shim_idx: 9, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm_bindgen__convert__closures_____invoke__hba68ab5ace6cabed);
             return ret;
         },
